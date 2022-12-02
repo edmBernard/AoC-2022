660ec5d25114bc2893c6d9241bbbdf319ce2f95f
Erwan BERNARD <bernard.erwan@gmail.com>
2022-12-02 08:16:25 +0100

    day02: clean

--- src/day02.rs
+++ src/day02.rs
@@ -3,55 +3,46 @@ use std::path::Path;
 
 use crate::Result;
 
-//A for Rock, B for Paper, and C for Scissors
-//X for Rock, Y for Paper, and Z for Scissors
-fn to_int(choice: &str) -> u64 {
+// A for Rock, B for Paper, and C for Scissors
+// X for Rock, Y for Paper, and Z for Scissors
+fn to_int(choice: &str) -> Result<u64> {
   match choice {
-    "A" => 0,
-    "B" => 1,
-    "C" => 2,
-    "X" => 0,
-    "Y" => 1,
-    "Z" => 2,
-    _ => 1000000,
+    "A" => Ok(0),
+    "B" => Ok(1),
+    "C" => Ok(2),
+    "X" => Ok(0),
+    "Y" => Ok(1),
+    "Z" => Ok(2),
+    character => Err(format!("Invalid character : {}", character).into()),
   }
 }
 
-fn result_part1(opponent: &str, me: &str) -> u64 {
-  let opponent_value = to_int(opponent);
-  let me_value = to_int(me);
-  let outcome_score = if me_value == opponent_value {
-    3
-  } else if (opponent_value + 1) % 3 == me_value {
-    // (opponent_value + 1) % 3 is the choice that beat opponent_value
-    6
-  } else {
-    0
-  };
-  (me_value+1) + outcome_score
+fn result_part1(opponent: &str, mine: &str) -> Result<u64> {
+  let opponent_choice = to_int(opponent)?;
+  let my_choice = to_int(mine)?;
+  // I add +3 because I use unsigned integer and I don't want to have negative after substraction
+  let outcome_score = (((my_choice + 1) + 3 - (opponent_choice + 1) + 1) % 3) * 3;
+  Ok((my_choice+1) + outcome_score)
 }
 
-fn result_part2(opponent: &str, me: &str) -> u64 {
-  let opponent_value = to_int(opponent);
+fn result_part2(opponent: &str, mine: &str) ->  Result<u64> {
+  let opponent_choice = to_int(opponent)?;
   // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
   // 0 means you need to lose, 1 means you need to end the round in a draw, and 2 means you need to win
-  let outcome = to_int(me);
-  let choice_score = if outcome == 1 {
-    opponent_value+1
-  } else if outcome == 2 {
-    ((opponent_value+1) % 3) + 1
-  } else {
-    ((opponent_value+2) % 3) + 1
-  };
-  choice_score + outcome * 3
+  let outcome = to_int(mine)?;
+
+  // I add +3 because I use unsigned integer and I don't want to have negative after substraction
+  let choice_score = ((opponent_choice + outcome + 3 - 1) % 3) + 1;
+  Ok(choice_score + outcome * 3)
 }
+
 pub fn day02(filename: &Path) -> Result<[u64; 2]> {
   let mut part1 = 0;
   let mut part2 = 0;
   for line in std::fs::read_to_string(filename)?.lines() {
     let line_str = line.split(" ").collect::<Vec<_>>();
-    part1 += result_part1(line_str[0], line_str[1]);
-    part2 += result_part2(line_str[0], line_str[1]);
+    part1 += result_part1(line_str[0], line_str[1])?;
+    part2 += result_part2(line_str[0], line_str[1])?;
   }
 
   Ok([part1, part2])
