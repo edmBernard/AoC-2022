c7ebcaed759b525e06dcc900cc8247c8acd09d79
Erwan BERNARD <bernard.erwan@gmail.com>
2022-12-02 08:57:47 +0100

    day02: add speed

--- src/day02.rs
+++ src/day02.rs
@@ -5,32 +5,47 @@ use crate::Result;
 
 // A for Rock, B for Paper, and C for Scissors
 // X for Rock, Y for Paper, and Z for Scissors
-fn to_int(choice: &str) -> Result<u64> {
-  match choice {
-    "A" => Ok(0),
-    "B" => Ok(1),
-    "C" => Ok(2),
-    "X" => Ok(0),
-    "Y" => Ok(1),
-    "Z" => Ok(2),
-    character => Err(format!("Invalid character : {}", character).into()),
+// X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+trait InterpreteToInt {
+  fn interprete(&self) -> Result<u64>;
+}
+
+impl InterpreteToInt for &str {
+  fn interprete(&self) -> Result<u64> {
+    match *self {
+      "A" => Ok(0),
+      "B" => Ok(1),
+      "C" => Ok(2),
+      "X" => Ok(0),
+      "Y" => Ok(1),
+      "Z" => Ok(2),
+      character => Err(format!("Invalid character : {}", character).into()),
+    }
   }
 }
 
-fn result_part1(opponent: &str, mine: &str) -> Result<u64> {
-  let opponent_choice = to_int(opponent)?;
-  let my_choice = to_int(mine)?;
+impl InterpreteToInt for char {
+  fn interprete(&self) -> Result<u64> {
+    match self {
+      'A' => Ok(0),
+      'B' => Ok(1),
+      'C' => Ok(2),
+      'X' => Ok(0),
+      'Y' => Ok(1),
+      'Z' => Ok(2),
+      character => Err(format!("Invalid character : {}", character).into()),
+    }
+  }
+}
+
+fn result_part1(opponent_choice: u64, my_choice: u64) -> Result<u64> {
   // I add +3 because I use unsigned integer and I don't want to have negative after substraction
   let outcome_score = (((my_choice + 1) + 3 - (opponent_choice + 1) + 1) % 3) * 3;
   Ok((my_choice+1) + outcome_score)
 }
 
-fn result_part2(opponent: &str, mine: &str) ->  Result<u64> {
-  let opponent_choice = to_int(opponent)?;
-  // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+fn result_part2(opponent_choice: u64, outcome: u64) -> Result<u64> {
   // 0 means you need to lose, 1 means you need to end the round in a draw, and 2 means you need to win
-  let outcome = to_int(mine)?;
-
   // I add +3 because I use unsigned integer and I don't want to have negative after substraction
   let choice_score = ((opponent_choice + outcome + 3 - 1) % 3) + 1;
   Ok(choice_score + outcome * 3)
@@ -40,9 +55,22 @@ pub fn day02(filename: &Path) -> Result<[u64; 2]> {
   let mut part1 = 0;
   let mut part2 = 0;
   for line in std::fs::read_to_string(filename)?.lines() {
-    let line_str = line.split(" ").collect::<Vec<_>>();
+    let line_str = line.split(" ").filter_map(|v| v.interprete().ok()).collect::<Vec<_>>();
     part1 += result_part1(line_str[0], line_str[1])?;
     part2 += result_part2(line_str[0], line_str[1])?;
+  }
+
+  Ok([part1, part2])
+}
+
+pub fn day02_speed(filename: &Path) -> Result<[u64; 2]> {
+  let mut part1 = 0;
+  let mut part2 = 0;
+  for line in std::fs::read_to_string(filename)?.lines() {
+    let value1 = line.chars().nth(0).expect("Can't index/Out of bound").interprete()?;
+    let value2 = line.chars().nth(2).expect("Can't index/Out of bound").interprete()?;
+    part1 += result_part1(value1, value2)?;
+    part2 += result_part2(value1, value2)?;
   }
 
   Ok([part1, part2])
@@ -57,5 +85,7 @@ mod tests {
   add_test!(
     main:   day02, "data/day02.txt",                  [11475, 16862];
     test1:  day02, "data/day02_test1.txt",            [15, 12];
+    main:   day02_speed, "data/day02.txt",            [11475, 16862];
+    test1:  day02_speed, "data/day02_test1.txt",      [15, 12];
   );
 }
