874fcc8162fc74f73e0892923e81a685703356a7
Erwan BERNARD <bernard.erwan@gmail.com>
2022-12-05 19:42:14 +0100

    day05: add part2

--- src/day05.rs
+++ src/day05.rs
@@ -17,7 +17,8 @@ pub fn day05(filename: &Path) -> Result<[u64; 2]> {
   let mut bin_size = 0;
   let mut state = State::ParseHeader;
   let regex = Regex::new(r"move (\d+) from (\d+) to (\d+)")?;
-  let mut board : Vec<Vec<char>> = Vec::new();
+  let mut board_part1 : Vec<Vec<char>> = Vec::new();
+  let mut board_part2 : Vec<Vec<char>> = Vec::new();
   // Ugly state machine because I don't find a way to continue the content_iterator in another loop
   for line in content_iterator {
     match state {
@@ -32,14 +33,15 @@ pub fn day05(filename: &Path) -> Result<[u64; 2]> {
       }
       State::InterpreteHeader => {
         // Interprete header
-        board = vec![Vec::new(); bin_size as usize];
+        board_part1 = vec![Vec::new(); bin_size as usize];
         for header_line in header.iter().rev() {
           for (index, value) in header_line.chars().skip(1).step_by(4).enumerate() {
             if value != ' ' {
-              board[index].push(value);
+              board_part1[index].push(value);
             }
           }
         }
+        board_part2 = board_part1.clone();
         state = State::ParseMovement;
       }
       State::ParseMovement => {
@@ -47,19 +49,26 @@ pub fn day05(filename: &Path) -> Result<[u64; 2]> {
         let quantity = cap.get(1).ok_or("Fail to capture 1")?.as_str().parse::<u64>()?;
         let src = cap.get(2).ok_or("Fail to capture 2")?.as_str().parse::<u64>()? - 1;
         let dst = cap.get(3).ok_or("Fail to capture 3")?.as_str().parse::<u64>()? - 1;
-        let mut temp = Vec::new();
+        let mut temp_part1 = Vec::new();
+        let mut temp_part2 = Vec::new();
         for _ in 0..quantity {
-          temp.push(board[src as usize].pop().ok_or("No enough value to pop")?);
+          temp_part1.push(board_part1[src as usize].pop().ok_or("No enough value to pop")?);
+          temp_part2.push(board_part2[src as usize].pop().ok_or("No enough value to pop")?);
         }
-        for elem in temp {
-          board[dst as usize].push(elem);
+        for elem in temp_part1 {
+          board_part1[dst as usize].push(elem);
+        }
+        for elem in temp_part2.into_iter().rev() {
+          board_part2[dst as usize].push(elem);
         }
       }
     }
   }
-  let part1 = board.iter().map(|stack| String::from(stack[stack.len()-1])).collect::<Vec<_>>().join("");
+  let part1 = board_part1.iter().map(|stack| String::from(stack[stack.len()-1])).collect::<Vec<_>>().join("");
+  let part2 = board_part2.iter().map(|stack| String::from(stack[stack.len()-1])).collect::<Vec<_>>().join("");
   // Result of part1 is a string
-  // println!("part1 {}", part1);
+  println!("part1 {} expected on test CMZ on real ZWHVFWQWW", part1);
+  println!("part2 {} expected on test MCD on real HZFZCCWWV", part2);
   Ok([0, 0])
 }
 
