97a91ea4e801de079ae4b3dc506705ea40314117
Erwan BERNARD <bernard.erwan@gmail.com>
2022-12-08 20:11:25 +0100

    day06: add faster solution

--- src/day06.rs
+++ src/day06.rs
@@ -15,8 +15,10 @@ pub fn day06(filename: &Path) -> Result<ReturnType> {
     .windows(4)
     .enumerate()
     .filter_map(|(index, chars)| {
-      for v in chars.iter().permutations(2).unique() {
-        if v[0] == v[1] {
+      let mut char_list = chars.to_owned();
+      char_list.sort();
+      for (v1, v2) in char_list.iter().tuple_windows::<(_, _)>() {
+        if v1 == v2 {
           return None;
         }
       }
@@ -31,8 +33,11 @@ pub fn day06(filename: &Path) -> Result<ReturnType> {
     .windows(14)
     .enumerate()
     .filter_map(|(index, chars)| {
-      for v in chars.iter().permutations(2).unique() {
-        if v[0] == v[1] {
+      let mut char_list = chars.to_owned();  // to_own is faster than .iter.collect
+      char_list.sort();
+      // tuple_windows is faster than using dedup and checking the length
+      for (v1, v2) in char_list.iter().tuple_windows::<(_, _)>() {
+        if v1 == v2 {
           return None;
         }
       }
@@ -44,42 +49,42 @@ pub fn day06(filename: &Path) -> Result<ReturnType> {
   Ok(ReturnType::Numeric(part1 as u64, part2 as u64))
 }
 
+
 pub fn day06_speed(filename: &Path) -> Result<ReturnType> {
   let file_content = std::fs::read_to_string(filename)?;
   let content = file_content.lines().next().ok_or("Empty File")?;
 
+  const MARKER_LEN: usize = 4;
   let part1 = content
     .chars()
     .collect::<Vec<_>>()
-    .windows(4)
+    .windows(MARKER_LEN)
     .enumerate()
-    .filter_map(|(index, chars)| {
-      let mut char_list = chars.iter().collect::<Vec<_>>();
-      char_list.sort();
-      char_list.dedup();
-      if char_list.len() < 4 {
-        return None;
+    .find_map(|(index, chars)| {
+      for i in 0..MARKER_LEN-1 {
+        if chars[i+1..].contains(&chars[i]) {
+          return None;
+        }
       }
-      Some(index + 4)
+      Some(index + MARKER_LEN)
     })
-    .next()
     .ok_or("No marker found")?;
 
+  const MESSAGE_LEN: usize = 14;
   let part2 = content
     .chars()
     .collect::<Vec<_>>()
-    .windows(14)
+    .windows(MESSAGE_LEN)
     .enumerate()
-    .filter_map(|(index, chars)| {
-      let mut char_list = chars.iter().collect::<Vec<_>>();
-      char_list.sort();
-      char_list.dedup();
-      if char_list.len() < 14 {
-        return None;
+    .find_map(|(index, chars)| {
+      // this simpler solution was so much faster
+      for i in 0..MESSAGE_LEN-1 {
+        if chars[i+1..].contains(&chars[i]) {
+          return None;
+        }
       }
-      Some(index + 14)
+      Some(index + MESSAGE_LEN)
     })
-    .next()
     .ok_or("No message found")?;
 
   Ok(ReturnType::Numeric(part1 as u64, part2 as u64))
@@ -94,5 +99,7 @@ mod tests {
   add_test!(
     main:   day06,        "data/day06.txt",              [1282, 3513];
     test1:  day06,        "data/day06_test1.txt",        [7, 19];
+    main:   day06_speed,  "data/day06.txt",              [1282, 3513];
+    test1:  day06_speed,  "data/day06_test1.txt",        [7, 19];
   );
 }
