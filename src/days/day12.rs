806d2b1897b2d6293b27f2c0c6a159c37bdd97a9
Erwan BERNARD <bernard.erwan@gmail.com>
2022-12-12 08:56:01 +0100

    day12: part2

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -50,10 +50,12 @@ impl std::fmt::Display for Board {
 }
 
 fn get_neighbor(x: usize, y: usize, width: usize, height: usize) -> [(usize, usize); 4] {
-  return [(if x < width-1 {x+1} else {x}, y),
-          (if x > 0 {x-1} else {x}, y),
-          (x, if y < height-1 {y+1} else {y}),
-          (x, if y > 0 {y-1} else {y})];
+  return [
+    (if x < width - 1 { x + 1 } else { x }, y),
+    (if x > 0 { x - 1 } else { x }, y),
+    (x, if y < height - 1 { y + 1 } else { y }),
+    (x, if y > 0 { y - 1 } else { y }),
+  ];
 }
 
 pub fn day12(filename: &Path) -> Result<ReturnType> {
@@ -80,47 +82,65 @@ pub fn day12(filename: &Path) -> Result<ReturnType> {
     }
   }
 
-  let mut frontier = Vec::new();
-  frontier.push((start, 0));
-  let mut came_from = HashMap::new();
-  came_from.insert(start, None);
-  let mut cost_so_far = HashMap::new();
-  cost_so_far.insert(start, 0);
+  let mut part1 = 0;
+  let mut part2 = 0;
+  for is_part1 in [false, true] {
+    // TODO: make part1 and part2 in one loop
+    let mut frontier = Vec::new();
+    frontier.push((start, 0));
+    let mut came_from = HashMap::new();
+    // TODO: maybe we don't need came_from as we only need the len() not the path
+    came_from.insert(start, None);
+    let mut cost_so_far = HashMap::new();
+    cost_so_far.insert(start, 0);
 
-  while let Some((current, _)) = frontier.pop() {
-    // if end == current {
-    //   break;
-    // }
-
-    for next in get_neighbor(current.0, current.1, board.width, board.get_height()) {
-
-      if board.get(next.0, next.1) > board.get(current.0, current.1) + 1 {
-        continue;
+    while let Some((current, _)) = frontier.pop() {
+      if end == current {
+        break;
       }
-      let current_cost = cost_so_far.get(&current).ok_or("Previous pos not found")?;
-      let new_cost =  current_cost + 1;
 
-      if !came_from.contains_key(&next) || new_cost < *cost_so_far.get(&next).ok_or("Previous pos not found")? {
-        cost_so_far.insert(next, new_cost);
-        frontier.push((next, new_cost));
-        came_from.insert(next, Some(current));
+      for next in get_neighbor(current.0, current.1, board.width, board.get_height()) {
+        if board.get(next.0, next.1) > board.get(current.0, current.1) + 1 {
+          continue;
+        }
+        let current_cost = cost_so_far.get(&current).ok_or("Previous pos not found")?;
+
+        let new_cost = if is_part1 {
+          current_cost + 1
+        } else {
+          // if the elevation is 0 (aka. a) we put a cost of 0.
+          if board.get(current.0, current.1) == 0 {
+            0
+          } else {
+            current_cost + 1
+          }
+        };
+
+        if !came_from.contains_key(&next) || new_cost < *cost_so_far.get(&next).ok_or("Previous pos not found")? {
+          cost_so_far.insert(next, new_cost);
+          frontier.push((next, new_cost));
+          came_from.insert(next, Some(current));
+        }
       }
+      frontier.sort_by(|a, b| b.1.cmp(&a.1));
     }
-    frontier.sort_by(|a, b| b.1.cmp(&a.1));
-  }
 
-  let mut current = end;
-  let mut path = Vec::new();
-  while current != start {
-    path.push(current);
-    let Some(&Some(temp)) = came_from.get(&current) else {
+    let mut current = end;
+    let mut path = Vec::new();
+    while current != start {
+      path.push(current);
+      let Some(&Some(temp)) = came_from.get(&current) else {
       panic!("No source found")
     };
-    current = temp;
+      current = temp;
+    }
+    if is_part1 {
+      part1 = path.len();
+    } else {
+      part2 = cost_so_far.get(&end).ok_or("No end value")? + 1;
+    }
   }
-
-  let part1 = path.len();
-  Ok(ReturnType::Numeric(part1 as u64, 2))
+  Ok(ReturnType::Numeric(part1 as u64, part2 as u64))
 }
 
 #[cfg(test)]
@@ -130,7 +150,7 @@ mod tests {
 
   #[rustfmt::skip::macros(add_test)]
   add_test!(
-    main:   day12,        "data/day12.txt",       [437, 2];
-    test1:  day12,        "data/day12_test1.txt", [31, 2];
+    main:   day12,        "data/day12.txt",       [437, 430];
+    test1:  day12,        "data/day12_test1.txt", [31, 29];
   );
 }
