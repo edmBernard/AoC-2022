a00d46f0cef7a3ebc168f7a182aad1394dcc1ba2
Erwan BERNARD <bernard.erwan@gmail.com>
2022-12-16 13:08:34 +0100

    day16: still don't work

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -8,7 +8,16 @@ use std::path::Path;
 use crate::utils::ReturnType;
 use crate::Result;
 
-fn go_deeper(valve_graph: &HashMap<&str, Vec<&str>>, valve_flow: &HashMap<&str, i32>, current_id: &str, current_score: i32, current_flow_rate: i32, count: i32, valve_status: &HashMap<&str, bool>, cache: &mut HashMap<(i32, String, i32), i32>) -> i32 {
+fn go_deeper(
+  valve_graph: &HashMap<&str, Vec<&str>>,
+  valve_flow: &HashMap<&str, i32>,
+  current_id: &str,
+  current_score: i32,
+  current_flow_rate: i32,
+  count: i32,
+  valve_status: &HashSet<&str>,
+  cache: &mut HashMap<(i32, String, i32), i32>,
+) -> i32 {
   if count > 30 {
     return current_score;
   }
@@ -16,30 +25,47 @@ fn go_deeper(valve_graph: &HashMap<&str, Vec<&str>>, valve_flow: &HashMap<&str, 
   let cache_key = (count, current_id.to_string(), current_flow_rate);
   if let Some(cached_value) = cache.get(&cache_key) {
     if *cached_value >= current_score {
-      return 0;
+      // println!("flow_rate:{} score:{} cached:{}", current_flow_rate, current_score, *cached_value);
+      return current_score;
     }
   }
   cache.insert(cache_key, current_score);
 
   // println!("id:{}, count:{}, score:{}, flow_rate:{}", current_id, count, current_score, current_flow_rate);
+  let new_score = current_score + current_flow_rate;
 
-  let mut score = current_score;
+  let mut score = new_score;
   // choice 1: open valve
   let current_flow = *valve_flow.get(&current_id).unwrap();
-  if *valve_status.get(&current_id).unwrap() == false && current_flow != 0 {
+  if !valve_status.contains(&current_id) && current_flow != 0 {
     let mut valve_status_clone = valve_status.clone();
-    *valve_status_clone.get_mut(&current_id).unwrap() = true;
+    valve_status_clone.insert(&current_id);
 
-    let new_score = current_score + current_flow_rate;
     let new_flow_rate = current_flow_rate + current_flow;
-    score = go_deeper(valve_graph, valve_flow, current_id, new_score, new_flow_rate, count + 1, &valve_status_clone, cache);
+    score = go_deeper(
+      valve_graph,
+      valve_flow,
+      current_id,
+      new_score,
+      new_flow_rate,
+      count + 1,
+      &valve_status_clone,
+      cache,
+    );
   }
 
   // choice 2: next valve
   for valve in valve_graph.get(&current_id).unwrap() {
-    let new_score = current_score + current_flow_rate;
-    let score_next = go_deeper(valve_graph, valve_flow, valve, new_score, current_flow_rate, count + 1, valve_status, cache);
-    score = score.max(score_next);
+    score = go_deeper(
+      valve_graph,
+      valve_flow,
+      valve,
+      new_score,
+      current_flow_rate,
+      count + 1,
+      valve_status,
+      cache,
+    ).max(score);
   }
   score
 }
@@ -47,7 +73,7 @@ fn go_deeper(valve_graph: &HashMap<&str, Vec<&str>>, valve_flow: &HashMap<&str, 
 pub fn day16(filename: &Path) -> Result<ReturnType> {
   let mut valve_graph = HashMap::new();
   let mut valve_flow = HashMap::new();
-  let mut valve_status = HashMap::new();
+  let mut valve_status = HashSet::new();
   // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
   let re = Regex::new(r"([A-Z]{2}).*=(\d+).+?((?:,? [A-Z]{2})+)")?;
   let content = std::fs::read_to_string(filename)?;
@@ -69,7 +95,7 @@ pub fn day16(filename: &Path) -> Result<ReturnType> {
 
     valve_graph.insert(current_valve, next_valve);
     valve_flow.insert(current_valve, flow_rate);
-    valve_status.insert(current_valve, false);
+    // valve_status.insert(current_valve, false);
   }
   let content = std::fs::read_to_string(filename)?;
   for line in content.lines() {
@@ -77,13 +103,18 @@ pub fn day16(filename: &Path) -> Result<ReturnType> {
     let current_valve = caps.get(1).ok_or("Fail to capture valve id")?.as_str();
 
     let list = valve_graph.get(current_valve).unwrap().iter().join(", ");
-    println!("Valve {} has flow rate={}; tunnels lead to valves {}", current_valve, valve_flow.get(current_valve).unwrap(), list);
-    // println!("ID:{}, Flow:{:?}, Next:[{:?}]", k, valve_flow.get(k), v)
+    println!(
+      "Valve {} has flow rate={}; tunnels lead to valves {}",
+      current_valve,
+      valve_flow.get(current_valve).unwrap(),
+      list
+    );
   }
 
   let mut cache = HashMap::new();
   let part1 = go_deeper(&valve_graph, &valve_flow, "AA", 0, 0, 1, &valve_status, &mut cache);
 
+  println!("IT DON'T WORK, I WAS OFF BY 1 POINT");
   Ok(ReturnType::Numeric(part1 as u64, 2 as u64))
 }
 
